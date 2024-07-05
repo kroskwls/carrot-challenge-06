04fe2b3e7d5b7e24a27f741f50d99f1463b3a329
Dongjin Cho <kroskwls@gmail.com>
2024-07-05 17:27:52 +0900

    change:create svg icon tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import { useFormState } from "react-dom";
 import { handleForm } from "./actions";
+import { CheckCircle } from "@/icons/svg-outline";
 
 export default function Home() {
   const [state, dispatch] = useFormState(handleForm, null);
@@ -18,9 +19,7 @@ export default function Home() {
       </form>
       {state?.success ? (
         <div className="flex gap-4 bg-green-500 mt-4 p-5 rounded-xl font-bold items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-          </svg>
+          <CheckCircle />
           <div>Welcome back!</div>
         </div>
       ) : null}
